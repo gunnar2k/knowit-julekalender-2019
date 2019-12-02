84e5ca81c0bd299d5ff5f99ea0f58efd0d5547b3
gunnar2k <gunnarfornes@gmail.com>
2019-12-02 08:55:21 +0100

    :sparkles: posted solution

--- day-01/solve.ex
+++ day-01/solve.ex
@@ -1,4 +1,51 @@
-# Solution will be posted after 06 AM on dec 2nd :)
-
 defmodule Day1 do
+  def solve([sheep_today | rest], %{
+        days: days,
+        hunger: hunger,
+        rage: rage,
+        extra_sheep: extra_sheep
+      } = _state) do
+    leftovers = sheep_today + extra_sheep - hunger
+
+    cond do
+      rage == 5 ->
+        days - 1
+
+      leftovers < 0 ->
+        solve(rest, %{
+          days: days + 1,
+          hunger: hunger - 1,
+          rage: rage + 1,
+          extra_sheep: 0
+        })
+
+      leftovers >= 0 ->
+        solve(rest, %{
+          days: days + 1,
+          hunger: hunger + 1,
+          rage: 0,
+          extra_sheep: leftovers
+        })
+
+      true ->
+        IO.puts("Unhandled case")
+    end
+  end
 end
+
+{:ok, raw_input} = File.read("sau.txt")
+
+input =
+  String.split(raw_input, " ")
+  |> Enum.map(&Integer.parse/1)
+  |> Enum.map(&elem(&1, 0))
+
+initial_state = %{
+  days: 0,
+  hunger: 50,
+  rage: 0,
+  extra_sheep: 0
+}
+
+Day1.solve(input, initial_state)
+|> IO.puts()
